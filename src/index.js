e1dd840f01db354cc8f9bfea7ef58438550ff4f7
KektorFlex <dzhachvadze@gmail.com>
2023-08-13 18:41:40 +0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 
 require('normalize.css/normalize.css');
-require('./styles/index.scss');
+// require('./styles/index.scss');
+require('./styles/index.css');
 
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
 
-    const pluginsTriggerElement = document.getElementById('plugins-trigger');
-    const pluginsElement = document.getElementById('plugins');
+//     const pluginsTriggerElement = document.getElementById('plugins-trigger');
+//     const pluginsElement = document.getElementById('plugins');
 
-    const pluginsVisibleClass = "splash-overview-plugins__list--visible";
+//     const pluginsVisibleClass = "splash-overview-plugins__list--visible";
 
-    pluginsTriggerElement.onclick = () => {
-        pluginsElement.classList.toggle(pluginsVisibleClass);
-    }
-});
+//     pluginsTriggerElement.onclick = () => {
+//         pluginsElement.classList.toggle(pluginsVisibleClass);
+//     }
+// });
